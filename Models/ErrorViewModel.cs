383fcddc2180a756ff27b0a4860bcf5c4a194a02
exp <experiment@localhost.localdomain>
2021-01-14 22:33:48 +0100

    Initial commit: let the battle begin

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace trumpsters_vs_the_world.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
